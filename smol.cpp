08579bee4d03029c415f06237fb9b1b141af80c6
AnishGRao <anishr@umich.edu>
2021-01-13 00:01:18 -0800

    pc reset

---
+++ smol.cpp
@@ -0,0 +1,11 @@
+int height = 8, width = 10, bmask = 95 /*conversions from uppercase to lowercase*/, t=65 /*extra space on board slots*/, 
+                temp1, temp2, temp3, temp4, temp5, temp6, /*etc*/, holder=32;
+char board[95] = ;
+
+
+int main(){
+    while(true){
+
+    }
+    return 0;
+}
